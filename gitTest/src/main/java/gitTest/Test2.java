1845868e4559b86c8e3099fcd2cac3c9109220a7
qkekw <dogcow909@naver.com>
2021-12-01 12:16:56 +0900

    add test class

---
+++ gitTest/src/main/java/gitTest/Test2.java
@@ -0,0 +1,8 @@
+package gitTest;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Test2 클래스 SH branch에서 작업함");
+	}
+
+}
--- gitTest/src/main/java/gitTest/TestMain.java
+++ gitTest/src/main/java/gitTest/TestMain.java
@@ -3,6 +3,7 @@ package gitTest;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Test");
+		System.out.println("새로운 branch (sh)에서 작업함");
 	}
 
 }
